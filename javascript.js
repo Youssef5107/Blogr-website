510c48220babb8ff7236ae3033210b195fbc228d
youssef <ybelalmarzouk@gmail.com>
2024-08-19 07:04:34 +0300

    feat:add arrow function

--- javascript.js
+++ javascript.js
@@ -1,6 +1,20 @@
-function showMeun(clickedBtn) {
+function showMeun(clickedBtn, btnArrow) {
 	var allNav = document.querySelectorAll("nav");
 	var nav = document.getElementById(clickedBtn);
+	var arrow = document.getElementById(btnArrow);
+	var allArrows = document.querySelectorAll(".icon-arrow-light");
+
+	if (arrow.classList.contains("active")) {
+		arrow.classList.remove("active");
+		return
+	}
+	else {
+		for (i = 0; i < allArrows.length; i++) {
+			allArrows[i].classList.remove("active");
+		}
+		arrow.classList.add("active");
+	}
+
 	if (nav.style.opacity == 1) {
 		nav.style.opacity = 0;
 		return
